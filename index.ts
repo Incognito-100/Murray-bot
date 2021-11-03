c530aeadaf182cd9292a76d6445fb96b07fb78f8
Izak <izakvanheerden1@gmail.com>
2021-11-03 22:45:40 +0200

    new command handler implemented

--- index.ts
+++ index.ts
@@ -1,29 +1,21 @@
 import DiscordJS, { Intents } from 'discord.js'
+import WOKCommands from 'wokcommands'
+import path from 'path'
 import dotenv from 'dotenv'
 dotenv.config()
 
 const client = new DiscordJS.Client({
-  intents:[Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_BANS, Intents.FLAGS.GUILD_INVITES, Intents.FLAGS.GUILD_VOICE_STATES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Intents.FLAGS.DIRECT_MESSAGES, Intents.FLAGS.DIRECT_MESSAGE_REACTIONS],
+  intents:[Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
 })
 
 client.on('ready', () => {
   console.log('the bot is ready')
 
-  const guildId = '838405624822890506'
-  const guild =client.guilds.cache.get(guildId)
-  let commands
-
-  if (guild) {
-    commands = guild.commands
-  } else {
-    commands = client.application?.commands
-  }
-
-  commands?.create({
-    name: 'ping',
-    description: 'Replies with pong',
+  new WOKCommands(client, {
+    commandsDir: path.join(__dirname, 'commands'),
+    typeScript: true,
+    testServers: ['838405624822890506'],
   })
 })
-
 
 client.login(process.env.TOKEN)
